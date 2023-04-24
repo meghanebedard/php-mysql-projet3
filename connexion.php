6878004d31eaf446c67ddc5e193c0feac14d6302
Méghane Bédard <meg_bed@outlook.com>
2023-04-24 10:22:56 -0400

    dbco
    connexion to colnet database

---
+++ connexion.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "alfy";
+$password = "nufhig-Cepxob-jenzu1";
+$port = 8889;
+
+$dbname = "Colnet";
+
+try {
+    $dbco = new PDO("mysql:host=$servername;dbname=$dbname;port=$port", $username, $password);
+    $dbco -> setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "CONNEXION RÉUSSIE";
+} catch (PDOException $e) {
+    echo "Erreur : " . $e -> getMessage();
+}
+?>
