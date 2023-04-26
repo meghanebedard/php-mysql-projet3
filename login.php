9d0acbfa714ca2c20452349312095a28b7156bcd
Méghane Bédard <meg_bed@outlook.com>
2023-04-26 09:25:38 -0400

    fix css login

--- login.php
+++ login.php
@@ -1,49 +1,55 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Créer un compte</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="styles.css">
-    </head>
-    <body id="login">
-        <div class="center">
-            <?php
-            include "header.php";
-            ?>
-            <h2>Veuillez vous connecter</h2>
+
+<head>
+    <title>Créer un compte</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body id="login">
+    <div class="center">
+        <?php
+        include "header.php";
+        ?>
+        <h2>Veuillez vous connecter</h2>
+    </div>
+    <form method="post" action="register.php">
+        <div class="champ">
+            <label>Username : </label>
+            <input class="field" type="text" name="username" pattern="[A-Za-z0-9]{2,30}" required />
         </div>
-        <form method="post" action="register.php">
+        <div class="champ">
+            <label>Mot de passe : </label>
+            <input class="field" type="text" name="motdepasse" placeholder="**********" pattern="[A-Za-z0-9 !@#$%?&*()éèà^.,+=_-]{2,30}" required />
+        </div>
+        <div class="boutton">
             <div class="champ">
-                <label>Username : </label>
-                <input class="field" type="text" name="username" pattern="[A-Za-z0-9]{2,30}" required/>
-            </div>
-            <div class="champ">
-                <label>Mot de passe : </label>
-                <input class="field" type="text" name="motdepasse" placeholder="**********" pattern="[A-Za-z0-9 !@#$%?&*()éèà^.,+=_-]{2,30}" required/>
-            </div>
-            <div class="champ boutton">
                 <input class="btn" type="submit" name="login" value="Se connecter" />
             </div>
-            <div class="champ boutton">
+            <div class="champ">
                 <input class="btn" type="submit" name="link" value="Créer un compte" onclick="location.href='register.php';" />
             </div>
-        </form>
-        <?php
-        include "connexion.php";
-        if (isset($_POST["register"])) {
-            $username = $_POST["username"];
-            $motdepasse = $_POST["motdepasse"];
-
-            $registerAccount = $dbco->prepare(
+        </div>
+    </form>
+    <?php
+    include "connexion.php";
+    if (isset($_POST["register"])) {
+        $username = $_POST["username"];
+        $motdepasse = $_POST["motdepasse"];
+/*
+        $registerAccount = $dbco->prepare(
             "INSERT INTO utilisateurs (nomComplet, username, codePostal, email, motDePasse)
             VALUES
             ('$nom', '$username', '$codepostal', '$email', '$motdepasse')
-            ");
-            $registerAccount -> execute();
-        }
-        ?>
-        <?php
-        include "footer.php";
-        ?>
-    </body>
-</html> 
+            "
+        );
+        $registerAccount->execute();*/
+    }
+    ?>
+    <?php
+    include "footer.php";
+    ?>
+</body>
+
+</html>
