2d305de0dcf671daa755561fa8f8e1b69437780f
Méghane Bédard <meg_bed@outlook.com>
2023-04-26 09:41:31 -0400

    format

--- connexion.php
+++ connexion.php
@@ -8,8 +8,8 @@ $dbname = "Colnet";
 
 try {
     $dbco = new PDO("mysql:host=$servername;dbname=$dbname;port=$port", $username, $password);
-    $dbco -> setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Erreur : " . $e -> getMessage();
+    echo "Erreur : " . $e->getMessage();
 }
 ?>
